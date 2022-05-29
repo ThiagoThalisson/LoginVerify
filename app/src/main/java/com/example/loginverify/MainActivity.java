b7040fb49001b19cc2b4be31fb73e94b10713c82
AtomicFeasT <atomic-feast@hotmail.com>
2022-05-28 21:26:52 -0300

    second commit

--- app/src/main/java/com/example/loginverify/MainActivity.java
+++ app/src/main/java/com/example/loginverify/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.loginverify;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    public Button loginBtn;
+    public EditText email = null;
+    public EditText password = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email.getText() == null || password.getText() == null) {
+                    System.out.println("Something's not right");
+                } else {
+                    Intent welcomeScreen = new Intent(getApplicationContext(), WelcomeScreen.class);
+
+                    startActivity(welcomeScreen);
+                }
+
+            }
+        });
     }
+
 }
